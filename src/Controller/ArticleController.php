4e03e55a5aa6bf5f428f6bd69be7a333fafc5059
mliebaut <melusine.liebaut@gmail.com>
2025-02-03 12:15:34 +0100

    Fix routes bug, add style in article pages and FAQ

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,25 +1,34 @@
 <?php
 
+
 namespace App\Controller;
 
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
 {
-    #[Route('/list-article', name: 'app_article_list')]
+
+    #[Route('/list-article', name: 'app_article_list', methods: ['GET'])]
     public function list(): Response
     {
-        return $this->render('list.html.twig', [
+        return $this->render('article/list.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
-    #[Route('/article/{slug}', name: 'app_article')]
-    public function article(Article $article): Response
+    #[Route('/article/{slug}', name: 'app_article', methods: ['GET'])]
+    public function article(#[MapEntity(mapping: ['slug' => 'slug'])] Article $article): Response
     {
-        return $this->render('list.html.twig', [
-            'controller_name' => 'ArticleController',
+        return $this->render('article/article.html.twig', [
+            //'controller_name' => 'ArticleController',
+            'article' => $article,
         ]);
     }
+
+
 }
